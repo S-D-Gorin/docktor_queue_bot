194d7f94cc6ad390d89e19dd593a6e14e33f42fc
Sergey Dmitrievich <129999088+S-D-Gorin@users.noreply.github.com>
2026-02-03 01:13:43 +0300

    upd help text

--- bot/bot.py
+++ bot/bot.py
@@ -47,8 +47,8 @@ TEMPLATE_TEXT = (
 
 HELP_TEXT = (
     "Возникли проблемы?\n"
-    "Связаться с разработчиком: https://t.me/sprotect_bots\n"
-    "Сайт https://yatolkosprosit.sprotectbots.ru/"
+    "Связаться с разработчиком: https://sdgor.apercode.com\n"
+    "О проекте: https://yatolkosprosit.sprotectbots.ru/"
 )
 # =========================
 # Парсинг и валидация
